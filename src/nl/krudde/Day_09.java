ad25a16dc766406761991791c72c20e157a08d2c
Fitzgerald Krudde <fitzgerald.krudde@ing.com>
2019-12-09 19:00:10 +0100

    day 9: extended (and cleaned/refactored) the IntCode computer

---
+++ src/nl/krudde/Day_09.java
@@ -0,0 +1,203 @@
+package nl.krudde;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import static java.util.stream.Collectors.toList;
+
+@Data
+@Builder
+class IntcodeV4 {
+    final private int MODE_POSITION = 0;
+    final private int MODE_IMMEDIATE = 1;
+    final private int MODE_RELATIVE = 2;
+
+    private double[] program;
+    int position;
+    int relativeBase = 0;
+
+    @Builder.Default
+    private boolean halted = false;
+    @Builder.Default
+    Stack<Integer> input = new Stack<>();
+    @Builder.Default
+    private boolean initialised = false;
+
+    public void addInput(Integer input) {
+        this.input.push(input);
+    }
+
+    public void run() {
+        int opcode;
+        double firstParameter, secondParameter;
+        boolean waitingForInput = false;
+
+        if (!initialised) {
+            double[] inputProgram = program;
+            // make space for 256K memory above the program
+            program = new double[program.length + 256 * 1024];
+            // copy the program
+            System.arraycopy(inputProgram, 0, program, 0, inputProgram.length);
+            initialised = true;
+        }
+
+        while (!halted) {
+            opcode = getOpcode(position);
+            switch (opcode) {
+                case 1 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    int writePosition = getPosition(position, 3);
+                    program[writePosition] = firstParameter + secondParameter;
+                    position += 4;
+                }
+                case 2 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    int writePosition = getPosition(position, 3);
+                    program[writePosition] = firstParameter * secondParameter;
+                    position += 4;
+                }
+                case 3 -> {
+                    if (input.empty()) {
+                        throw new IllegalStateException("input empty");
+                    }
+                    int writePosition = getPosition(position, 1);
+                    program[writePosition] = input.pop();
+                    position += 2;
+                }
+                case 4 -> {
+                    firstParameter = getParameter(position, 1);
+                    System.out.printf("%.0f ", firstParameter);
+                    position += 2;
+                }
+                case 5 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    if (firstParameter != 0) {
+                        position = (int) secondParameter;
+                    } else {
+                        position += 3;
+                    }
+                }
+                case 6 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    if (firstParameter == 0) {
+                        position = (int) secondParameter;
+                    } else {
+                        position += 3;
+                    }
+                }
+                case 7 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    int writePosition = getPosition(position, 3);
+                    program[writePosition] = firstParameter < secondParameter ? 1 : 0;
+                    position += 4;
+                }
+                case 8 -> {
+                    firstParameter = getParameter(position, 1);
+                    secondParameter = getParameter(position, 2);
+                    int writePosition = getPosition(position, 3);
+                    program[writePosition] = firstParameter == secondParameter ? 1 : 0;
+                    position += 4;
+                }
+                case 9 -> {
+                    firstParameter = getParameter(position, 1);
+                    relativeBase += firstParameter;
+                    position += 2;
+                }
+                default -> throw new IllegalStateException("unknown opcode: " + opcode);
+            }
+            halted = program[position] == 99;
+        }
+    }
+
+    private int getOpcode(int position) {
+        return (int) (program[position] % 100);
+    }
+
+    private double getParameter(int positionInstruction, int index) {
+        return program[getPosition(positionInstruction, index)];
+    }
+
+    private int getPosition(int positionInstruction, int index) {
+        int instruction = (int) program[positionInstruction];
+        int mode = (int) ((instruction / (10 * (int) Math.pow(10, index))) % 10);
+        return switch (mode) {
+            case MODE_IMMEDIATE -> positionInstruction + index;
+            case MODE_POSITION -> (int) program[positionInstruction + index];
+            case MODE_RELATIVE -> (int) (program[positionInstruction + index] + relativeBase);
+            default -> throw new IllegalStateException("unknown mode: " + mode);
+        };
+    }
+}
+
+public class Day_09 {
+
+    final static String DEFAULT_FILENAME = new Object() {
+    }.getClass().getEnclosingClass().getSimpleName().toLowerCase().replace("_0", "_") + ".txt";
+
+    public static void main(String[] args) throws IOException {
+        LocalTime start = LocalTime.now();
+
+        String input = readFile(args).get(0);
+
+        // part 1
+        System.out.println("\npart 1: ");
+        double[] intCodeProgram = Arrays.stream(input.split(","))
+                .mapToDouble(Double::valueOf)
+                .toArray();
+
+        IntcodeV4 intcode = new IntcodeV4.IntcodeV4Builder()
+                .program(intCodeProgram)
+                .build();
+        intcode.addInput(1);
+        intcode.run();
+        System.out.println();
+
+        LocalTime finish = LocalTime.now();
+        System.out.println("duration (ms): " + Duration.between(start, finish).toMillis());
+
+        // part 2
+        start = LocalTime.now();
+        System.out.println("\npart 2: ");
+
+        intcode = new IntcodeV4.IntcodeV4Builder()
+                .program(intCodeProgram)
+                .build();
+        intcode.addInput(2);
+        intcode.run();
+        System.out.println();
+
+        finish = LocalTime.now();
+        System.out.println("duration (ms): " + Duration.between(start, finish).toMillis());
+    }
+
+    private static List<String> readFile(String[] args) throws IOException {
+        String fileName;
+        if (args.length == 0) {
+            fileName = DEFAULT_FILENAME;
+        } else {
+            fileName = args[0];
+        }
+
+        System.out.println("reading file: " + fileName);
+        // get the input lines
+        List<String> input = Files.lines(Path.of(fileName)).collect(toList());
+        System.out.println(String.format("read file: %s (#lines: %d)", fileName, input.size()));
+
+        return input;
+    }
+
+}
